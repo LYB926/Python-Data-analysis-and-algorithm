d37ca0c68849b7c2980033ea05a04a5d0c64581a
Wisp <zk1019@outlook.com>
2023-10-05 15:24:47 +0800

    Update Q1.py

--- H2/Q1.py
+++ H2/Q1.py
@@ -6,29 +6,46 @@ class MyArray:
             self.array = []
         else:
             for _ in args:
-                if isinstance(_, (int, float)):
+                if not isinstance(_, (int, float)): # 检查数组元素类型是否合法
                     print ('Type error: element type should be int or float')
                     return
             self.array = list(args)
-    
+            print(self)
+       
+    def __str__(self):
+        return str(self.array)
+    __repr__ = __str__
+
+    # 运算符重载
     def __mul__(self, x):
-        if isinstance(x, (int, float)):
-            t = tuple([_*x for _ in self])
-            return (MyArray(t))
-        elif isinstance(x, MyArray):
+        if isinstance(x, (int, float)): # 数乘的情况
+            t = [_*x for _ in self.array]
+            v = MyArray()
+            v.array = t
+            return (v)
+        elif isinstance(x, MyArray):    # 数组相乘的情况
             if len(self.array) == len(x.array):
-                t = tuple([a*b for a in self.array for b in x.array])
-                return (MyArray(t))
-            else:
+                t = [a*b for a,b in zip(self.array, x.array)]
+                v = MyArray()
+                v.array = t
+                return (v)
+            else:                       # 检查输入数组长度是否相等
                 print ('Inconsistent array length.')
                 return
-        else:
+        else:                           # 考虑操作数是否合法
             print ('Type error: operand type should be int, float or MyArray.')
             return
-        
+
+# 功能测试部分代码
+print('数组声明：')
 x = MyArray(1, 2, -1, 3)
 y = MyArray(4, 3, 2, 1)
-z = x * y
-            
-
+z = MyArray(2, 5, 8)
+r = MyArray(7, 2, 'z')
+print('\n数组运算：')
+print(x * 5)
+print(x * y)
+print(x * z)
+print(x * 'x')
+# print (x * y)
             
