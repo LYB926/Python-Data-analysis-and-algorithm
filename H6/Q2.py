8c72fd37287b0542a9a6c3de6e52dd08d882a275
Wisp <zk1019@outlook.com>
2023-12-19 22:02:59 +0800

    Finished Q2-4.

--- H6/Q2.py
+++ H6/Q2.py
@@ -4,14 +4,47 @@
 （1）[1,2], [2,3], [3,10], N =10
 （2）[1,2], [1,10], [2,4], [2,10], [3,4], [3,12], N=12
 '''
-
-clips = [[1, 2], [2, 3], [3, 10]]
-N = 10
+# clips = [[1, 2], [2, 3], [3, 10]]
+# N = 10
+clips = [[1,2], [1,10], [2,4], [2,10], [3,4], [3,12]]
+N = 12
 print('Clips = ', clips, '\nN = ', N)
 
-clips_sorted = clips.sort()
-clips_selected = []
-t = 0
+# 使用二进制穷举法
+num_clips = len(clips)
+min_len = float('inf')
+min_dura = float('inf')
+selected_clips = []
 
-while t < N:
-    
+for i in range(0, (1 << num_clips)):
+    tmp_clips = []
+    for j in range(0, num_clips):
+        if (i & (1 << j)):
+            tmp_clips.append(clips[j])
+
+    # 判定穷举得到的片段是否合法
+    tmp_len = len(tmp_clips)
+    if (tmp_len == 0):
+        continue
+    tmp_clips.sort()
+    flag = True
+    if (tmp_clips[0][0] != 1) or (tmp_clips[tmp_len-1][1] < N):
+        flag = False
+    for k in range(0, tmp_len-1):
+        if (tmp_clips[k][1] < (tmp_clips[k+1][0]-1)):
+            flag = False
+
+    # 如果序列合法，检查视频数量是否最少且时长（dura）最短
+    if flag == True:
+        lenc = tmp_len
+        dura = 0
+        for u in range(0, tmp_len):
+            dura = dura + (tmp_clips[u][1] - tmp_clips[u][0])
+        if (lenc < min_len):
+            min_len, min_dura, selected_clips = lenc, dura, tmp_clips
+        if ((lenc == min_len) and (dura < min_dura)):
+            min_len, min_dura, selected_clips = lenc, dura, tmp_clips
+
+print ('\n最小的视频数量: {}'.format(len(selected_clips)))
+print (selected_clips)
+        
---
+++ H6/Q4.py
@@ -0,0 +1,28 @@
+'''
+4.	给定一个包含N个不等元素的数组，输出其中位数。要求采用O(NlogN)复杂度的算法实现。要求测试下例：
+（1）[4,10,3,7,5,2]
+（2）[8,2,3,5,7,6,11,14,1,9,13,10,4,12,15]
+'''
+# 快速排序时间复杂度：O(NlogN)
+def quick_sort(arr):
+    if len(arr) >= 2:
+        key = arr[len(arr)//2] 
+        right, left = [], []  
+        arr.remove(key)  
+
+        for i in arr:
+            if i < key:
+                left.append(i)
+            else:
+                right.append(i)
+        return quick_sort(left)+[key]+quick_sort(right)
+    else:
+        return arr
+    
+# arr = [4,10,3,7,5,2]
+arr = [8,2,3,5,7,6,11,14,1,9,13,10,4,12,15]
+print('arr = ', arr)
+
+arr_sorted = quick_sort(arr)
+median = arr_sorted[int(len(arr_sorted)/2)]
+print('中位数为： {}'.format(median))
