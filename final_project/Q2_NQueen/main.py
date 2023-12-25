ee71a8fa0b9bc37cb804e453565a474ed9d6d5b6
Wisp <zk1019@outlook.com>
2023-12-25 17:00:33 +0800

    Added ASCII arts title for fun :smiley:

--- final_project/Q2_NQueen/main.py
+++ final_project/Q2_NQueen/main.py
@@ -1,6 +1,15 @@
 from NQueen import NQueen
 from NQueenGUI import NQueenGUI
 
+title = '''
+    _   __      ____
+   / | / /     / __ \__  _____  ___  ____  _____
+  /  |/ /_____/ / / / / / / _ \/ _ \/ __ \/ ___/
+ / /|  /_____/ /_/ / /_/ /  __/  __/ / / (__  )
+/_/ |_/      \___\_\__,_/\___/\___/_/ /_/____/
+'''
+print(title)
+
 # 实例化
 nqueen_instance = NQueen()
 GUI_instance = NQueenGUI()
